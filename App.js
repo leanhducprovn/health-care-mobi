a6dfae26294e5755f6c19c070c53a0d8d6d4a302
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-22 12:15:32 +0700

    update

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <WebView source={{ html: "<iFrame src='https://project.thanglongit.vn/' />" }} />
+    <WebView source={{ html: `<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body><style>iframe{width:100%;border:0}::-webkit-scrollbar{display:none}</style><div style='position:fixed;bottom:0;right:0;top:0;left:0;z-index:999;'><iframe height='100%' src='https://project.thanglongit.vn/' width='100%'/></div></body</html>` }} />
   );
 }
-
