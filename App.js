521de1e27ace598a7a373a5f95157df5fc31a818
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-22 12:50:01 +0700

    update

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, StyleSheet } from 'react-native'
-import { WebView } from 'react-native-webview'; 
-
+import { WebView } from 'react-native-webview';
+import * as AuthSession from 'expo-auth-session';
 
 export default function App() {
   return (
     <View style = {styles.container}>
-      <WebView source = {require("./index.html")} />
+      <WebView
+        userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"
+        originWhitelist = {["https://*", "http://*", "file://*", "sms://*"]}
+        source = {require("./index.html")} />
       <StatusBar style="auto" />
     </View>
   );
