10211480629884517b1f163303c71f64ebbdac50
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-22 11:56:37 +0700

    update

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview'; 
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lê Anh Đức</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView source={{ html: "<iFrame src='https://project.thanglongit.vn/' />" }} />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
