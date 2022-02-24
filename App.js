5e6bd291457890bb54cc1c082510faf8c0fa5641
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-24 20:23:59 +0700

    update

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { StyleSheet } from 'react-native'
 import { WebView, WebViewNavigation } from 'react-native-webview';
 
+// Send the cookie information back to the mobile app
+const CHECK_COOKIE = `
+  ReactNativeWebView.postMessage("Cookie: " + document.cookie);
+  true;
+`;
+
 export default function App() {
   return (
       <WebView
