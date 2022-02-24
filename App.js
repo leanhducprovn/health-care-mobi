d612eff30a459241ea87dabce646801ba13b2b29
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-24 19:15:16 +0700

    update

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native'
-import { WebView } from 'react-native-webview';
+import { WebView, WebViewNavigation } from 'react-native-webview';
 
 export default function App() {
   return (
