e523cc03c125a81a87978ea8d9a5eba6814693b1
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-01 21:10:30 +0700

    update

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import { WebView, WebViewNavigation } from "react-native-webview";
+import CookieManager from "@react-native-community/cookies";
+
+// Send the cookie information back to the mobile app
+const CHECK_COOKIE = `
+	ReactNativeWebView.postMessage("Cookie: " + document.cookie);
+	true;
+`;
+
+const onNavigationStateChange = (navigationState) => {
+    // Check cookies every time URL changes
+    if (webViewRef.current) {
+        webViewRef.current.injectJavaScript(CHECK_COOKIE);
+    }
+};
+
+const onMessage = async (event) => {
+    const { data } = event.nativeEvent;
+
+    if (data.includes("Cookie:")) {
+        // process the cookies
+        const storedCookies = await CookieManager.get("https://mywebapp.com/login", true);
+    }
+};
 
 export default function App() {
-	return <WebView style={styles.container} source={require("./index.html")} />;
+    return <WebView style={styles.container} source={require("./index.html")} />;
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
+    container: {
+        flex: 1,
+    },
 });
